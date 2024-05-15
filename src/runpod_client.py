e0384d05dcd81850ad18d72ea07d6405c78b6fbd
wellflat <wellflat@gmail.com>
2024-05-15 23:53:04 +0900

    added sample client code

---
+++ src/runpod_client.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+import json
+import os
+from pathlib import Path
+
+from gql import Client
+from gql import gql
+from gql.transport.aiohttp import AIOHTTPTransport
+from gql.transport.exceptions import TransportQueryError
+
+if __name__ == "__main__":
+    try:
+        api_key = os.getenv("RUNPOD_API_KEY")
+        transport = AIOHTTPTransport(
+          url=f"https://api.runpod.io/graphql?api_key={api_key}",
+        )
+        client = Client(transport=transport)
+
+        with Path.open(Path("serverless.gql")) as f:
+            lines = f.read()
+
+        query = gql(lines)
+        result = client.execute(query)
+        print(json.dumps(result, indent=2))
+    except TransportQueryError as e:
+        print(e)

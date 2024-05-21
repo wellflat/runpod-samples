989beb09fefe6bace95b2eea2f916ed7d14b2293
wellflat <wellflat@gmail.com>
2024-05-21 23:51:32 +0900

    modified deploy part

--- src/runpod_client.py
+++ src/runpod_client.py
@@ -12,19 +12,17 @@ from gql import Client
 from gql import gql
 from gql.transport.aiohttp import AIOHTTPTransport
 from gql.transport.exceptions import TransportQueryError
+from gql.transport.exceptions import TransportServerError
 
 
 def parse_args() -> argparse.Namespace:
-    parser = argparse.ArgumentParser(description="RunPod Deploy")
+    parser = argparse.ArgumentParser(description="Deploy container to RunPod Serverless")
     parser.add_argument("--api_key", type=str, default=None, help="RunPod API Key")
     parser.add_argument("--image_name", required=True, type=str, help="Target container image name")
     return parser.parse_args()
 
 async def request_save_template(gql_path: Path, args: argparse.Namespace) -> dict[str, Any]:
     api_key = args.api_key if args.api_key else os.getenv("RUNPOD_API_KEY")
-    transport = AIOHTTPTransport(
-        url=f"https://api.runpod.io/graphql?api_key={api_key}",
-    )
     image_name = args.image_name
     with Path.open(gql_path) as f:
         lines = f.read()
@@ -54,6 +52,10 @@ async def request_save_template(gql_path: Path, args: argparse.Namespace) -> dic
             },
         },
     }
+
+    transport = AIOHTTPTransport(
+        url=f"https://api.runpod.io/graphql?api_key={api_key}",
+    )
     async with Client(transport=transport) as session:
         query = gql(lines)
         return await session.execute(query, variable_values=params, parse_result=True)
@@ -63,9 +65,11 @@ async def main() -> None:
         args = parse_args()
         gql_path = Path(__file__).parent / "runpod_save_template2.gql"
         result = await request_save_template(gql_path, args)
-        print(json.dumps(result, indent=2))
+        sys.stdout.write(json.dumps(result, indent=2))
     except TransportQueryError as e:
         ## TODO logging
+        sys.stderr.write(str(e))
+    except TransportServerError as e:
         sys.stderr.write(str(e))
 
 asyncio.run(main())

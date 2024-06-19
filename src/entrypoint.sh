ed082989923cf203d37052bf57ca7bbbf0f81ac2
wellflat <wellflat@gmail.com>
2024-06-19 22:55:15 +0900

    modified action

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -2,7 +2,8 @@
 
 echo "start model repository clone"
 git lfs install
-git clone ${MODEL_REPO_URL}
+#git clone ${MODEL_REPO_URL}
+git clone https://github.com/wellflat/runpod-samples
 echo "clone completed"
 ls sample-models/models
 echo "start handler"

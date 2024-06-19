485662bed7270c503f6978cdc91312a51d6b3f71
wellflat <wellflat@gmail.com>
2024-06-19 19:10:45 +0900

    modified entrypoint

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
 
+echo "start model repository clone"
+git lfs install
+git clone ${MODEL_REPO_URL}
+echo "clone completed"
+ls sample-models/models
+echo "start handler"
 python3 -u rp_handler.py

7a6c3e4a630d8de0af89daa21cd7914610211c7e
wellflat <wellflat@gmail.com>
2024-06-20 19:15:13 +0900

    using huggingface-cli at entrypoint

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -1,13 +1,18 @@
 #!/bin/sh
 
-echo "start model repository clone"
-git lfs install
-echo ${MODEL_REPO_URL}
-git clone "${MODEL_REPO_URL}"
-echo "clone completed"
-cd sample-models
-git checkout "${SUBMODULE_COMMIT_ID}"
-ls models
-cd -
+#echo "start model repository clone"
+#git lfs install
+#echo ${MODEL_REPO_URL}
+#git clone "${MODEL_REPO_URL}"
+#echo "clone completed"
+#cd sample-models
+#git checkout "${SUBMODULE_COMMIT_ID}"
+#ls models
+#cd -
+
+export HF_HUB_ENABLE_HF_TRANSFER=1
+huggingface-cli download \
+    --token ${HF_TOKEN} \
+    --revision ${SUMODULE_COMMIT_ID}
 echo "start handler"
 python3 -u rp_handler.py

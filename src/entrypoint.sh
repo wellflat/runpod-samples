2157f326f281ec8b7c23fb157ea26e1963431e1e
wellflat <wellflat@gmail.com>
2024-06-20 19:20:27 +0900

    using huggingface-cli at entrypoint,fixed typo

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -13,6 +13,6 @@
 export HF_HUB_ENABLE_HF_TRANSFER=1
 huggingface-cli download \
     --token ${HF_TOKEN} \
-    --revision ${SUMODULE_COMMIT_ID}
+    --revision ${SUBODULE_COMMIT_ID}
 echo "start handler"
 python3 -u rp_handler.py

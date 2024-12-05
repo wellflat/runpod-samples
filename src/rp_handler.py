421706638dedae54a42de45dd216f7b16c2994f5
wellflat <wellflat@gmail.com>
2024-12-05 18:25:20 +0900

    added sentry test

--- src/rp_handler.py
+++ src/rp_handler.py
@@ -10,6 +10,7 @@ import sentry_sdk
 from sentry_sdk.integrations.loguru import LoguruIntegration
 from sentry_sdk.integrations.serverless import serverless_function
 from loguru import logger
+import sentry_sdk.profiler
 
 def process_input(input_data: dict[str, str]) -> dict[str, str]:
     transaction = sentry_sdk.start_transaction(op="process_input", name="process_input(transaction)")
@@ -21,16 +22,18 @@ def process_input(input_data: dict[str, str]) -> dict[str, str]:
     span = transaction.start_child(op="calculate_fibonacci", name=f"calculate_fibonacci({number})")
     fibonacci_result = calculate_fibonacci(number)
     span.finish()
-    span = transaction.start_child(op="hash", description=f"hashsha256({number})")
+    span = transaction.start_child(op="hash", name=f"hashsha256({number})")
     digest = hashlib.sha256(f"{greeting}-{fibonacci_result}".encode()).hexdigest()
     span.finish()
     transaction.finish()
     return { "greeting": greeting, "fibonacci": str(fibonacci_result), "digest": digest }
 
+
 def calculate_fibonacci(n: int) -> int:
     if n <= 1:
         return n
     return calculate_fibonacci(n - 1) + calculate_fibonacci(n - 2)
+
 
 ### RunPod Handler
 @serverless_function
@@ -49,9 +52,11 @@ if __name__ == "__main__":
         traces_sample_rate=1.0,
         # Set profiles_sample_rate to 1.0 to profile 100% of sampled transactions.
         # We recommend adjusting this value in production.
-        profiles_sample_rate=1.0
+        #profiles_sample_rate=1.0,
     )
+    sentry_sdk.profiler.start_profiler()
     runpod.serverless.start({"handler": handler})
+    sentry_sdk.profiler.stop_profiler()
     
     
 
